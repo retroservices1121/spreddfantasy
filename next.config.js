204e611b009dc9fa287ab6a9423ae4768c023ce4
retroservices1121 <76626942+retroservices1121@users.noreply.github.com>
2025-09-27 21:10:11 -0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  env: {
+    CUSTOM_KEY: process.env.CUSTOM_KEY,
+  },
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
+  images: {
+    domains: ['kalshi.com'],
+  },
+};
+
+module.exports = nextConfig;
