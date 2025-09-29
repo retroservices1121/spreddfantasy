abf4cbead4831a45394e3c45a2380d5eda8258a7
retroservices1121 <76626942+retroservices1121@users.noreply.github.com>
2025-09-28 22:40:33 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  env: {
-    CUSTOM_KEY: process.env.CUSTOM_KEY,
-  },
   webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
